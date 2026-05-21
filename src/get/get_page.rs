6b340f7fc8553860e1bd1b8b5d7e48373374ae7f
Jason DeFillipo <jasond3663@gmail.com>
2026-05-21 00:47:05 -0700

    Caches api results during tests to reduce repeated calls

--- src/get/get_page.rs
+++ src/get/get_page.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+use std::{hash::{DefaultHasher, Hash, Hasher}, path::PathBuf};
+
 use serde_json::Value;
 
 use super::Dnd5eapiError;
@@ -9,9 +12,46 @@ pub async fn get_page(path: String) -> Result<reqwest::Response, reqwest::Error>
 }
 
 pub async fn get_raw_json(path: String) -> Result<serde_json::Value, Dnd5eapiError> {
-    let json = get_page(path).await?.json::<Value>().await?;
+    #[cfg(test)]
+    if let Some(s) = get_cached(&path) {
+        return Ok(s)
+    }
+    let json = get_page(path.clone()).await?.json::<Value>().await?;
+    #[cfg(test)]
+    set_cached(path, json.clone());
+
     Ok(json)
 }
+
+#[cfg(test)]
+fn get_cached(path: &String) -> Option<serde_json::Value> {
+    let mut hasher = DefaultHasher::new();
+    path.hash(&mut hasher);
+    let hashed_path = hasher.finish().to_string();
+    let mut path = PathBuf::from(".test_http_cache");
+    std::fs::create_dir_all(&path).ok()?;
+    path.push(&hashed_path);
+    if path.exists() {
+        let v_str = std::fs::read_to_string(path).ok()?;
+        let result = serde_json::from_str::<serde_json::Value>(&v_str).ok()?;
+        return Some(result)
+    }
+    None
+}
+
+#[cfg(test)]
+fn set_cached(path: String, val: serde_json::Value) -> Option<()> {
+    let mut hasher = DefaultHasher::new();
+    path.hash(&mut hasher);
+    let hashed_path = hasher.finish().to_string();
+    let mut path = PathBuf::from(".test_http_cache");
+    std::fs::create_dir_all(&path).ok()?;
+    path.push(hashed_path);
+    let body = serde_json::to_string(&val).ok()?;
+    std::fs::write(&path, &body).ok()?;
+    Some(())
+}
+
 
 #[cfg(test)]
 mod tests {
